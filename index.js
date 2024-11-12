a91ddcd171d97e4d16c49043219f4bba9c8b7593
ZOYA BEZOYAN <zoya1905bezoyan@gmail.com>
2024-11-12 13:14:31 +0400

    Use Redis as a message broker

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import { createServer } from 'http';
 import staticHandler from 'serve-handler';
 import { WebSocketServer } from 'ws';
+import Redis from 'ioredis';
+
+const redisOptions = {
+  password: '', // Replace with your actual Redis password
+};
+
+const redisSub = new Redis(redisOptions);
+const redisPub = new Redis(redisOptions);
 
 const server = createServer((req, res) => {
   return staticHandler(req, res, { public: 'www' });
@@ -12,16 +20,18 @@ wss.on('connection', client => {
   console.log('Client connected');
   client.on('message', msg => {
     console.log(`Message: ${msg}`);
+    redisPub.publish('chat_messages', msg);
     broadcast(msg);
   });
 });
 
-function broadcast(msg) {
+redisSub.subscribe('chat_messages');
+redisSub.on('message', (channel, msg) => {
   for (const client of wss.clients) {
     if (client.readyState === client.OPEN) {
       client.send(msg);
     }
   }
-}
+});
 
 server.listen(process.argv[2] || 8080);
